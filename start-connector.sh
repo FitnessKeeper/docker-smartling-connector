693bc5abeae29d3dab64a61f084ad5aa8bb7e0bd
John Stucklen <john.stucklen@runkeeper.com>
2017-04-26 14:46:25 -0400

    Ok, I think this will work now...

--- start-connector.sh
+++ start-connector.sh
@@ -10,7 +10,20 @@ envsubst < repo-connector-template.conf > cfg/repo-connector.conf
 JAVA_XMS=${JAVA_XMS:-1024M}
 JAVA_XMX=${JAVA_XMX:-3584M}
 
-trap "/opt/repo-connector/stop-connector.sh" HUP INT QUIT TERM
+# This signal handling is based on this blog post:
+# https://medium.com/@gchudnov/trapping-signals-in-docker-containers-7a57fdda7d86
+sig_handler()
+{
+    if [ ${pid} -ne 0  ]; then
+        kill -SIGTERM "${pid}"
+        wait "${pid}"
+    fi
+    exit 143; # 128 + 15 -- SIGTERM
+}
+
+# Setup handlers
+# On callback, kill the last background process, which is `tail -f /dev/null` and execute the specified handler
+trap "kill ${!}; /opt/repo-connector/stop-connector.sh" HUP INT QUIT TERM
 
 java -Xms${JAVA_XMS} -Xmx${JAVA_XMX} -jar /opt/repo-connector/${CONNECTOR_JAR_PREFIX}.jar -start &
 
@@ -18,7 +31,5 @@ pid="${!}"
 
 # Wait forever, but allow handling signals...
 while true; do
-    tail -f /dev/null & wait ${!}
+    tail -f /dev/null & wait "${!}"
 done
-
-wait ${pid}
